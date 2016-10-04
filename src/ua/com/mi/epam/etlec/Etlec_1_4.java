f81118a3fd2bbb61ea14dc23eb1dea58df15d825
West <igor.mashch@gmail.com>
2016-10-04 17:03:27 +0300

    Revert "Etlec_1_3_4_5"
    This reverts commit ef0536a4017fdf8214d2e41839df79f0633fdaa2.

---
+++ src/ua/com/mi/epam/etlec/Etlec_1_4.java
@@ -0,0 +1,28 @@
+package ua.com.mi.epam.etlec;
+
+public class Etlec_1_4 {
+	/*
+	 * —осчитать сумму р€да 1! - 2! + 3! Ц 4! + 5! - Е + n! дл€ заданного числа
+	 * n > 0. „ем шире диапазон возможных значений n, тем лучше. „исло n
+	 * задавать в коде программы
+	 */
+	public static int fact(int x) {
+		return (x < 1) ? 1 : x * fact(x - 1);
+	}
+
+	public static int factRow(int n) {
+		int result = 0;
+		for (int i = 1; i <= n; i++) {
+			if (i % 2 == 1) {
+				result += fact(i);
+			} else {
+				result -= fact(i);
+			}
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(factRow(10));
+	}
+}
