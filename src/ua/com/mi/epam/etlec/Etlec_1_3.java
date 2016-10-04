6eda7a5509762da4142ac878eb24771ec87546f9
West <igor.mashch@gmail.com>
2016-10-04 16:38:19 +0300

    Etlec_3_4_5

---
+++ src/ua/com/mi/epam/etlec/Etlec_1_3.java
@@ -0,0 +1,29 @@
+package ua.com.mi.epam.etlec;
+
+public class Etlec_1_3 {
+	/*
+	 * Написать программу проверки того, что заданное число Х – простое (т.е. не
+	 * делится без остатка ни на какие числа, кроме себя и 1). Число X задавать
+	 * в коде программы.
+	 */
+
+	public static boolean isSimple(int value) {
+		boolean result = true;
+		for (int i = value / 2; i > 1; i--) {
+			if (value % i == 0) {
+				result = false;
+				break;
+			}
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		int a = 1;
+		int b = a++;
+		int c = -a;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+}
---
+++ src/ua/com/mi/epam/etlec/Etlec_1_5.java
@@ -0,0 +1,27 @@
+package ua.com.mi.epam.etlec;
+
+import ua.dz.Etlec_1.Etlec_1_2;
+
+public class Etlec_1_5 {
+	public static int calculateLuckyTickets(int start, int end) {
+
+		int count = 0;
+		for (int six = start; six < end; six++) {
+			int lsiz = six / 1000;
+			int rsiz = six % 1000;
+			if (Etlec_1_2.sum(lsiz) == Etlec_1_2.sum(rsiz)) {
+				count++;
+			}
+		}
+		return count;
+
+	}
+
+	public static void main(String[] args) {
+		/*
+		 * Подсчитать, сколько шестизначных цифр имеют равную сумму трех первых
+		 * и трех последних цифр.
+		 */
+		System.out.println(calculateLuckyTickets(111111, 999999));
+	}
+}
