0d080ddd3c471d30e3b1580cfaaba65c7e0a924f
West <igor.mashch@gmail.com>
2016-10-04 16:35:04 +0300

    Etlec_1_2

---
+++ src/ua/com/mi/epam/etlec/Etlec_1_2.java
@@ -0,0 +1,21 @@
+package ua.com.mi.epam.etlec;
+
+public class Etlec_1_2 {
+	/*
+	 * Написать программу, которая находит сумму цифр произвольного целого
+	 * числа.
+	 */
+	public static int sum(int number) {
+		int sum = 0;
+		while (number != 0) {
+			int w = number % 10;
+			number = number / 10;
+			sum += w;
+		}
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(sum(1221));
+	}
+}
